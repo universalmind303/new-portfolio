b558be003f982e5685b4b1e348c03858acfce633
cgrinstead <universalmind.candy@gmail.com>
2018-08-26 22:42:30 -0500

    test

--- deploy/upload.py
+++ deploy/upload.py
@@ -1,7 +1,7 @@
 import os
 from env_vars import get
 
-os.system('service docker start')
+os.system('sudo systemctl start docker')
 os.system('docker build -f ../docker/devl.Dockerfile -t {image}:{tag} ../ '.format(**get()))
 os.system('docker tag {image} {host_name}/{project_id}/{image}:{tag}'.format(**get()))
 os.system('docker push {host_name}/{project_id}/{image}:{tag}'.format(**get()))
