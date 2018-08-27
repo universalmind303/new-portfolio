1ac700b2c49d920ada92aa5c37d19d0d1da07963
cgrinstead <universalmind.candy@gmail.com>
2018-08-26 20:42:40 -0500

    chore: update config and add custom docker image

--- deploy/env_vars.py
+++ deploy/env_vars.py
@@ -1,7 +1,7 @@
 import os
-
 def get():
     return {
+        'compute_zone': 'us-central1-a',
         'project_name': 'new-portfolio',
         'env':'devl',
         'project_id':'portfolio-203912',
---
+++ deploy/gcloud-install.sh
@@ -0,0 +1,16 @@
+# Update YUM with Cloud SDK repo information:
+set -eu
+
+tee -a /etc/yum.repos.d/google-cloud-sdk.repo << EOM
+[google-cloud-sdk]
+name=Google Cloud SDK
+baseurl=https://packages.cloud.google.com/yum/repos/cloud-sdk-el7-x86_64
+enabled=1
+gpgcheck=1
+repo_gpgcheck=1
+gpgkey=https://packages.cloud.google.com/yum/doc/yum-key.gpg
+       https://packages.cloud.google.com/yum/doc/rpm-package-key.gpg
+EOM
+
+yum install -y google-cloud-sdk
+# The indentation for the 2nd line of gpgkey is important.
