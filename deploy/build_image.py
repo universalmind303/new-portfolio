23e547e457481cee25edccac9669015c5f8dd140
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 18:59:03 -0500

    chore: code cleanup

--- deploy/build_image.py
+++
@@ -1,7 +0,0 @@
-import os 
-from env_vars import get
-
-
-os.system('''
-    docker build  --no-cache -t circleci-gcloud:latest  --build-arg GCLOUD_PROJECT_ID={project_id} --build-arg GCLOUD_CLUSTER_NAME={project_name} --build-arg GCLOUD_COMPUTE_ZONE={compute_zone} --build-arg GCLOUD_SERVICE_KEY="$(cat ~/.gcloud/key.json)" .
-'''.format(**get()))
