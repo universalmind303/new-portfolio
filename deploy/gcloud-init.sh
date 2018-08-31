0cec9ae4530406a22288b494ad88d77dc8c98a17
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 18:19:10 -0500

    chore: remove sudo

--- deploy/gcloud-init.sh
+++ deploy/gcloud-init.sh
@@ -6,11 +6,11 @@ set -eu
 
 mkdir -p $HOME/.gcloud
 echo $GCLOUD_SERVICE_KEY > ${HOME}/.gcloud/key.json
-sudo gcloud auth activate-service-account --key-file ${HOME}/.gcloud/key.json
-sudo gcloud config set project $GCLOUD_PROJECT_ID
-sudo gcloud --quiet config set container/cluster $GCLOUD_PROJECT_NAME
-sudo gcloud config set compute/zone ${GCLOUD_COMPUTE_ZONE}
-sudo gcloud --quiet container clusters get-credentials $GCLOUD_PROJECT_NAME
-sudo gcloud auth configure-docker
+gcloud auth activate-service-account --key-file ${HOME}/.gcloud/key.json
+gcloud config set project $GCLOUD_PROJECT_ID
+gcloud --quiet config set container/cluster $GCLOUD_PROJECT_NAME
+gcloud config set compute/zone ${GCLOUD_COMPUTE_ZONE}
+gcloud --quiet container clusters get-credentials $GCLOUD_PROJECT_NAME
+gcloud auth configure-docker
 
 
