57c58c613baae366d33dc084e95ec278f10aae99
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 17:45:13 -0500

    test: change py scripts to sh so circleci can use them easier

---
+++ deploy/set_env.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -eu
+
+ENV="devl"
+GCLOUD_COMPUTE_ZONE='us-central1-a'
+GCLOUD_PROJECT_NAME="new-portfolio"
+GCLOUD_PROJECT_ID="portfolio-203912"
+IMAGE="new-portfolio-${ENV}"
+TAG="latest"
+HOST_NAME="gcr.io"
---
+++ deploy/upload.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -eu
+
+. ./set_env.sh
+
+docker build -f ../docker/devl.Dockerfile -t ${IMAGE}:${TAG} ../
+docker tag ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
+docker push ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
