4da99cf9ab4b85d4f70bf8b28022bca8dbf34139
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 17:16:19 -0500

    test: test update:

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ steps to circleci deployment
 
 create gcloud kubernetes 
 create runtime image
-add env variable to circleci project
+
+store key to path ~/.gcloud/key.json
+add same key to env variable in circleci GUI
--- deploy/build_image.py
+++ deploy/build_image.py
@@ -3,5 +3,5 @@ from env_vars import get
 
 
 os.system('''
-    docker build  -t circleci-gcloud:latest  --build-arg GCLOUD_PROJECT_ID={project_id} --build-arg GCLOUD_CLUSTER_NAME={project_name} --build-arg GCLOUD_COMPUTE_ZONE={compute_zone} --build-arg GCLOUD_SERVICE_KEY="$(cat ~/.gcloud/key.json)" .
+    docker build  --no-cache -t circleci-gcloud:latest  --build-arg GCLOUD_PROJECT_ID={project_id} --build-arg GCLOUD_CLUSTER_NAME={project_name} --build-arg GCLOUD_COMPUTE_ZONE={compute_zone} --build-arg GCLOUD_SERVICE_KEY="$(cat ~/.gcloud/key.json)" .
 '''.format(**get()))
