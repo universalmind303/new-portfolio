e6c603e0340c18adda02ca985d4c519ec01a3a0c
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 20:54:29 -0500

    test: test with proper versioning of docker images

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -5,7 +5,12 @@ set -eu
 . ./set_env.sh
 
 docker build -f ../docker/devl.Dockerfile -t ${IMAGE}:${TAG} ../
+
+echo "tagging ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}"
 docker tag ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
+
+echo "pushing ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}"
 docker push ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
+
 echo "pushing to gcloud"
 gcloud docker --  push ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
