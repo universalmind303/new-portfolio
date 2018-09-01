d16af0d8b734f90a01b607957bd02161bc6b26ec
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 21:16:02 -0500

    test: try with temp hardcoded tag

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -4,7 +4,7 @@ set -eu
 
 . ./set_env.sh
 
-docker build -f ../docker/devl.Dockerfile -t ${IMAGE}:${TAG} ../
+docker build -f ../docker/devl.Dockerfile -t ${IMAGE}:latest ../
 
 echo "tagging ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}"
 docker tag ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
