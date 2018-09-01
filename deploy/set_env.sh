3c81261d95fca6bdc26421f819ed928e2fe44b92
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 21:19:08 -0500

    ci: implement hash for docker tag

--- deploy/set_env.sh
+++ deploy/set_env.sh
@@ -6,5 +6,5 @@ GCLOUD_COMPUTE_ZONE='us-central1-a'
 GCLOUD_PROJECT_NAME="new-portfolio"
 GCLOUD_PROJECT_ID="portfolio-203912"
 IMAGE="new-portfolio-${ENV}"
-TAG="temphardcodedtag"
+TAG="$(git log -1 --pretty=%H)"
 HOST_NAME="gcr.io"
