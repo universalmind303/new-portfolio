29ca90117bc992e9c293341cd447957b088b71d2
cgrinstead <universalmind.candy@gmail.com>
2018-08-31 21:03:40 -0500

    fix: fix upload.sh

--- deploy/upload.sh
+++ deploy/upload.sh
@@ -11,6 +11,3 @@ docker tag ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
 
 echo "pushing ${IMAGE} ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}"
 docker push ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
-
-echo "pushing to gcloud"
-gcloud docker --  push ${HOST_NAME}/${GCLOUD_PROJECT_ID}/${IMAGE}:${TAG}
